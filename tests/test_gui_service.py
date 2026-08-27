3dbd8901a0d3fbe926cdedce3a51137f0ddb46f1
Cartaz <prenc.francesco@gmail.com>
2026-08-27 09:40:49 +0200

    Expose pristine pilot in GUI catalog test

--- tests/test_gui_service.py
+++ tests/test_gui_service.py
@@ -25,6 +25,7 @@ def test_gui_catalog_switches_suite_without_mixing_tasks():
         "autonomy_expense_001",
         "autonomy_causal_gateway_001",
         "autonomy_runtime_investigation_001",
+        "long_horizon_pristine_001",
         "tool_use_config_001",
         "tool_use_branching_001",
         "tool_use_coverage_001",
